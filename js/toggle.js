5d9c2be1243a5294315037437337a60eebb9be6d
Kuba Rzepka <krzepka33@gmail.com>
2023-07-03 19:10:26 +0200

    Add: New CSS for popup, JS Script for links in popup

--- js/toggle.js
+++ js/toggle.js
@@ -341,4 +341,15 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+      (function () {
+          var ln = links[i];
+          var location = ln.href;
+          ln.onclick = function () {
+              chrome.tabs.create({active: true, url: location});
+          };
+      })();
+  }
+  
 });
